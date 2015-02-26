dc575d944465269cd6697570f3e16ea711a57805
Tigran Terian <tigran.terian@gmail.com>
2015-02-26 18:27:04 +0400

    Preparing for deployment

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,5 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+ Rails.application.config.assets.precompile += %w( flat-ui/dist/img/icons/svg/* )
+ Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
